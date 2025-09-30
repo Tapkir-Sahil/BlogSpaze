adb6d6f2278fa64ad17a5518c6d0a474b24a553e
sahil-tapkir <sahiltapkir07@gmail.com>
2025-09-30 16:23:00 +0530

    modified bootstrap files

--- react/myapp/src/App.jsx
+++ react/myapp/src/App.jsx
@@ -1,9 +1,16 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
 const App =()=>{
   return(
     <>
-    <h1>this is react</h1>
+      <Router>
+        <h1>this is react</h1>
+        <Routes>
+          <Route/>
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
 export default App;
