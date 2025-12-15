e52bb7e4e8336087cbe4b47e5816397e584abd9b
sahil-tapkir <sahiltapkir07@gmail.com>
2025-12-15 17:13:15 +0530

    added import files to server.js

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,12 @@
 import express from "express";
+import cors from "cors";
+import dotenv, { config } from "dotenv";
+import path from "path";
+import fs from "fs";
 
+// Project Imports
+import { connectDB } from "./db";
+
+dotenv.config()
+
+const app = express();
