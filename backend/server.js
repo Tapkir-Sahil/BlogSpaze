c4cde9cf020d85702573174ab17d53a4c33971a8
sahil-tapkir <sahiltapkir07@gmail.com>
2025-12-17 17:02:41 +0530

    modified server.js

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,38 @@
 import express from "express";
 import cors from "cors";
-import dotenv, { config } from "dotenv";
+import dotenv from "dotenv";
 import path from "path";
 import fs from "fs";
 
 // Project Imports
-import { connectDB } from "./db";
+import { connectDB } from "./db.js";
 
 dotenv.config()
 
 const app = express();
+
+//Middlewares
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+//Ensure public/uploads folder exists
+
+const uploadsDir = path.join(process.cwd(), "public","uploads");
+if(!fs.existsSync(uploadsDir)){
+  fs.mkdirSync(uploadsDir)
+}
+
+// Connect to mongodb, and start then start server
+await connectDB();
+
+//Health check of server
+app.get('/',(req,res)=>{
+  res.send('server is running')
+})
+const PORT = process.env.PORT || 5000;
+app.listen(PORT,()=>{
+  console.log(`server is running on http://localhost:${PORT}`)
+})
+
