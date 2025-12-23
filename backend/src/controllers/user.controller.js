10d89ff0297f4e0991fdb102dfb85edcea0e6f5a
sahil-tapkir <sahiltapkir07@gmail.com>
2025-12-23 12:31:43 +0530

    modified and complete user.controller.js

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -26,19 +26,45 @@ export const getUser = async (req, res) => {
 };
 
 //PUT /api/user/:id (to update //admin or owner)
-export const updateUser = async (req, res)=>{
-  try{
-    const {name,bio} = req.body;
+export const updateUser = async (req, res) => {
+  try {
+    const { name, bio } = req.body;
     const update = {};
-    if(name) update.name = name;
-    if(bio) update.bio = bio;
+    if (name) update.name = name;
+    if (bio) update.bio = bio;
+    //if file is uploaded by multer(req.file) upload it to cloudinary
+    if (req.file && req.file.path) {
+      const result = await cloudinary.uploader.upload(req.file.path, {
+        folder: "blogspaze/avatar",
+      });
+      update.profilepic = result.secure_url;
+    } else if (req.body.profilepic) {
+      update.profilepic = req.body.profilepic;
+    }
 
-    //if file is uploaded by multer(req.file) upload it to cloudinary 
-    
-    const user = await User.findById(req.params.id).select("-password")
-  }
-  catch(err){
+    const user = await User.findByIdAndUpdate(req.params.id, update, {
+      new: true,
+    }).select("-password");
+    if (!user) {
+      return res.status(404).json({ message: "user not found" });
+    }
+    res.json(user);
+  } catch (err) {
     console.log(err);
-    res.status(500).json({message: "internal server error"})
+    res.status(500).json({ message: "internal server error" });
   }
-}
+};
+
+//delete /api/user/:id (admin privilage)
+export const deleteUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+      return res.status(404).json({ message: "user not found" });
+    }
+    res.json({ message: "user delete success" });
+  } catch {
+    console.log(err);
+    res.status(500).json({ message: "internal server error" });
+  }
+};
