90bf10f4badc1afee8276cc2840bb61f21f1e80b
sahil-tapkir <sahiltapkir07@gmail.com>
2025-12-25 17:30:16 +0530

    completed with blog.controller.js

--- backend/src/controllers/blog.controller.js
+++ backend/src/controllers/blog.controller.js
@@ -2,6 +2,7 @@ import Blog from "../models/blog.model.js";
 import cloudinary from "../config/cloudinary.js";
 import fs from "fs";
 
+//to create a single blog
 export const createBlog = async (req, res) => {
   try {
     const { title, description, content } = req.body;
@@ -55,6 +56,67 @@ export const getBlog = async (req, res) => {
       return res.status(404).json({ message: "blog not found" });
     }
     res.json(blog);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "server error" });
+  }
+};
+
+//update the blog (author/admin)
+export const updateBlog = async (req, res) => {
+  try {
+    const blog = await Blog.findById(req.params.id);
+    if (!blog) {
+      return res.status(404).json({ message: "blog not found" });
+    }
+
+    //only author or admin can update
+    if (
+      blog.authorId.toString() !== req.user_id.toString() &&
+      req.user.role !== "admin"
+    ) {
+      return res.status(403).json({ message: "not authorized" });
+    }
+
+    const { title, description, content } = req.body;
+    if (title) blog.title = title;
+    if (description) blog.description = description;
+    if (content) blog.content = content;
+
+    //image upload
+    if (req.file && req.file.path) {
+      const result = await cloudinary.uploader.upload(req.file.path, {
+        folder: "blogspaze/posts",
+      });
+      blog = result.secure_url;
+      fs.unlinkSync(req.file.path);
+    }
+
+    await blog.save();
+    res.json(blog);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "server error" });
+  }
+};
+
+//to delete the blog
+export const deleteBlog = async (req, res) => {
+  try {
+    const blog = await Blog.findById(req.params.id);
+    if (!blog) {
+      return res.status(404).json({ message: "blog not found" });
+    }
+
+    if (
+      blog.authorId.toString() !== req.user_id.toString() &&
+      req.user.role !== "admin"
+    ) {
+      return res.status(403).json({ message: "not authorized" });
+    }
+
+    await Blog.findByIdAndDelete(req.params.id);
+    res.json({ message: "blog deleted" });
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "server error" });
